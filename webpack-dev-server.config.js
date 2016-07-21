277d6eca560590eb3a936d5ddb2e3c510ff151d9
JinLang <tommyhilinux@gmail.com>
2016-07-21 17:34:54 +0800

    init package.json

---
+++ webpack-dev-server.config.js
@@ -0,0 +1,69 @@
+var webpack = require('webpack');
+var path = require('path');
+var buildPath = path.resolve(__dirname, 'build');
+var nodeModulesPath = path.resolve(__dirname, 'node_modules');
+var TransferWebpackPlugin = require('transfer-webpack-plugin');
+
+var config = {
+
+  entry: [
+    'webpack/hot/dev-server',
+    'webpack/hot/only-dev-server',
+    path.join(__dirname, '/src/app/app.jsx')
+  ],
+
+  resolve: {
+    extensions: ["", ".js", ".jsx", ".css"]
+    //node_modules: ["web_modules", "node_modules"]  (Default Settings)
+  },
+
+  devServer:{
+    contentBase: 'src/www',  //Relative directory for base of server
+    devtool: 'eval',
+    hot: true,        //Live-reload
+    inline: true,
+    port: 3000,        //Port Number
+    host: '0.0.0.0'    //'localhost'  //Change to '0.0.0.0' for external facing server
+  },
+  devtool: 'eval',
+  output: {
+    path: buildPath,    //Path of output file
+    filename: 'app.js'
+  },
+  plugins: [
+    //Enables Hot Modules Replacement
+    new webpack.HotModuleReplacementPlugin(),
+    //Allows error warnings but does not stop compiling. Will remove when eslint is added
+    new webpack.NoErrorsPlugin(),
+    //Moves files
+    new TransferWebpackPlugin([
+      {from: 'www'}
+    ], path.resolve(__dirname, "src"))
+  ],
+  module: {
+
+    preLoaders: [
+      {
+        //Eslint loader
+        test: /\.(js|jsx)$/,
+        loader: 'eslint-loader',
+        include: [path.resolve(__dirname, "src/app")],
+        exclude: [nodeModulesPath]
+      },
+    ],
+    loaders: [
+      {
+        //React-hot loader and
+        test: /\.(js|jsx)$/,
+        loaders: ['react-hot', 'babel'], //react-hot is like browser sync and babel loads jsx and es6-7
+        exclude: [nodeModulesPath]
+      }
+    ]
+  },
+
+  eslint: {
+    configFile: '.eslintrc'
+  },
+};
+
+module.exports = config;
