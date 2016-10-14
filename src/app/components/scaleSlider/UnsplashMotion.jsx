cef43daab68fa9625668ef50d43837a4b495a281
JinLang <tommyhilinux@gmail.com>
2016-10-14 10:32:35 +0800

    change slide and scale

--- src/app/components/scaleSlider/UnsplashMotion.jsx
+++ src/app/components/scaleSlider/UnsplashMotion.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { spring, Motion, presets } from 'react-motion';
+import ImageList from './ImageList'
 
 import '../../style/unsplash.scss';
 import '../../style/scaleSlide.scss'
@@ -25,6 +26,8 @@ let winHeight = 0
 let winWidth = 0
 let firstTouchY = 0
 let photoProportion = 0
+let slideWidth = 0
+let slideState = 'crImg'      // 'crImg' 表示当前是全屏图片, 'crBlog' 表示当前转为 Blog 形式
 let springConfig = {
   stiffness: 600,
   damping: 50,
@@ -53,8 +56,8 @@ class Unsplash extends React.Component {
     super(props, context);
 
     this.handelImage = this.handelImage.bind(this)
-    this.handelImageClose = this.handelImageClose.bind(this)
-    this.hiddenPage = this.hiddenPage.bind(this)
+    // this.handelImageClose = this.handelImageClose.bind(this)
+    // this.hiddenPage = this.hiddenPage.bind(this)
     this.winTouchMove = this.winTouchMove.bind(this)
     this.winTouchEnd = this.winTouchEnd.bind(this)
 
@@ -64,6 +67,8 @@ class Unsplash extends React.Component {
       defaulScaleStyle : null,
       scaleStyle    : null,
       pageOpen      : false,
+      defaulSlideStyle: null,
+      slideStyle    : null,
     }
   }
 
@@ -74,8 +79,6 @@ class Unsplash extends React.Component {
 
   handelImage(e, img) {
     e.preventDefault()
-    // document.documentElement.style.overflow = 'hidden'
-    // document.body.style.overflow = 'hidden'
     window.addEventListener('touchstart', winTouchStart)
     window.addEventListener('touchmove', this.winTouchMove)
     window.addEventListener('touchend', this.winTouchEnd)
@@ -97,11 +100,23 @@ class Unsplash extends React.Component {
       left: spring( (photoProportion * winHeight - winWidth) / -2, springConfig),
     }
 
+    slideWidth = imageList.length * winWidth
+
+    let defaulSlideStyle = {
+      top: 0,
+      height: winHeight,
+      width: slideWidth,
+      left: 0,
+    }
+
     this.setState({
       scaleImg: img,
       defaulScaleStyle: defaulScaleStyle,
       scaleStyle: scaleStyle,
       showPhotoTilt: true,
+
+      defaulSlideStyle: defaulSlideStyle,
+      slideStyle: defaulSlideStyle,
     })
   }
 
@@ -135,15 +150,23 @@ class Unsplash extends React.Component {
 
       let left = (photoProportion * height - winWidth) / -2
 
-      let scaleStyle = {
+      // let scaleStyle = {
+      //   top: 0,
+      //   height: spring(height),
+      //   left: spring(left),
+      //   width: spring(width)
+      // }
+
+      let slideStyle = {
         top: 0,
+        left: 0,
         height: spring(height),
-        left: spring(left),
-        width: spring(width)
+        width: slideWidth, 
       }
 
       this.setState({
-        scaleStyle: scaleStyle
+        // scaleStyle: scaleStyle,
+        slideStyle: slideStyle,
       })
 
 
@@ -162,55 +185,54 @@ class Unsplash extends React.Component {
 
     if( touchYDelta < 0) {
       this.setState({
-        scaleStyle: {
+        slideStyle: {
           top: 0,
           height: spring(winWidth / photoProportion),
-          left: spring(0),
-          width: spring(winWidth)
+          left: 0,
+          width: slideWidth,
         }
       })
     }else{
       this.setState({
-        scaleStyle: {
+        slideStyle: {
           top: 0,
           height: spring(winHeight, springConfig),
-          left: spring((photoProportion * winHeight - winWidth) / -2, springConfig),
-          width: spring(photoProportion * winHeight, springConfig)
+          left: 0,
+          width: slideWidth,
         }
       })
     }
 
   }
 
-  handelImageClose(e) {
-    let imageData = this.state.defaulScaleStyle
+  // handelImageClose(e) {
+  //   let imageData = this.state.defaulScaleStyle
 
-    let scaleStyle = {
-      top: spring(imageData.top, springConfig),
-      left: spring(imageData.left, springConfig),
-      height: spring(imageData.height, springConfig),
-      width: spring(imageData.width, springConfig),
-    }
+  //   let scaleStyle = {
+  //     top: spring(imageData.top, springConfig),
+  //     left: spring(imageData.left, springConfig),
+  //     height: spring(imageData.height, springConfig),
+  //     width: spring(imageData.width, springConfig),
+  //   }
 
-    this.setState({
-      pageOpen: true,
-      scaleStyle: scaleStyle,
-    })
+  //   this.setState({
+  //     pageOpen: true,
+  //     scaleStyle: scaleStyle,
+  //   })
+  // }
 
-  }
-
-  hiddenPage() {
-    if(this.state.pageOpen){
-      this.setState({
-        pageOpen: false,
-        showPhotoTilt: false,
-        defaulScaleStyle: null,
-      })
-      window.removeEventListener('touchstart', winTouchStart)
-      window.removeEventListener('touchmove', this.winTouchMove)
-      window.removeEventListener('touchend', this.winTouchEnd)
-    }
-  }
+  // hiddenPage() {
+  //   if(this.state.pageOpen){
+  //     this.setState({
+  //       pageOpen: false,
+  //       showPhotoTilt: false,
+  //       defaulScaleStyle: null,
+  //     })
+  //     window.removeEventListener('touchstart', winTouchStart)
+  //     window.removeEventListener('touchmove', this.winTouchMove)
+  //     window.removeEventListener('touchend', this.winTouchEnd)
+  //   }
+  // }
 
   render() {
 
@@ -229,18 +251,45 @@ class Unsplash extends React.Component {
       transform: `translate3d(0, 0, 0)`,
     } : null
 
+    // { this.state.defaulScaleStyle ? (
+    //   <Motion defaultStyle={this.state.defaulScaleStyle} style={this.state.scaleStyle} onRest={this.hiddenPage}>
+    //     { 
+    //       interpolatingStyle => (
+    //         <div className='un-photo-scale' style={interpolatingStyle}>
+    //           <div className='un-photo-transform' style={transformStyle} onClick={this.handelImageClose}></div>
+    //         </div>
+    //       )
+    //     }
+    //   </Motion> ): null
+    // }
+
     let photoTilt = (
       <div className='un-photo-page' style={pageStyle}>
-        { this.state.defaulScaleStyle ? (
-          <Motion defaultStyle={this.state.defaulScaleStyle} style={this.state.scaleStyle} onRest={this.hiddenPage}>
-            { 
+        { states.defaulSlideStyle ? (
+          <Motion defaultStyle={states.defaulSlideStyle} style={states.slideStyle}>
+            {
               interpolatingStyle => (
-                <div className='un-photo-scale' style={interpolatingStyle}>
-                  <div className='un-photo-transform' style={transformStyle} onClick={this.handelImageClose}></div>
+                <div className='un-photo-slide-ul' style={interpolatingStyle}>
+                  {
+                    imageList.map((img, index)=>{
+                      let height = interpolatingStyle.height
+                      let slideImgStyle = {
+                        height: height,
+                        width: photoProportion * height,
+                        backgroundImage: `url(${img})`,
+                      }
+
+                      return (
+                        <div key={index} className='un-photo-slide-li'>
+                          <div className='un-photo-slide-li-img' style={slideImgStyle}></div>
+                        </div>
+                      )
+                    })
+                  }
                 </div>
               )
             }
-          </Motion> ): null
+          </Motion> ) : null
         }
         <div className={states.showPhotoTilt ? 'un-photo-blog un-show-blog' : 'un-photo-blog'}>
           <div className='un-blog-content'>
@@ -253,26 +302,11 @@ class Unsplash extends React.Component {
     return (
       <div className='un-container'>
         <div className='un-grid-single'>
-          {
-            imageList.map((img, index)=>{
-              let bgStyle = {
-                backgroundImage: `url(${img})`,
-                height: 250,                   // 需要检测图片原始高度再根据高宽比显示
-              }
-              return (
-                <div key={index} className='un-grid-item'>
-                  <div className='un-grid-pro'>
-                    <span>
-                      <img className='un-grid-portrait' src='https://images.unsplash.com/profile-1465711356836-8ac8cef76fff?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&cs=tinysrgb&fit=crop&h=32&w=32&s=ff0de384ba28a3440d35e8854edcf782' />
-                      <div className='un-grid-name'>Evan Kirby</div>
-                    </span>
-                  </div>
-                  <div className='un-grid-image' style={bgStyle} onClick={(e) => this.handelImage(e, img)}>
-                  </div>
-                </div>
-              )
-            })
-          }
+          <ImageList
+            imageList={imageList}
+            handelImage={this.handelImage}
+          >
+          </ImageList>
         </div>
         <span>
           {photoTilt}
